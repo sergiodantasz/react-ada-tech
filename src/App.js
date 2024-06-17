fa3bedc3690d6bd6a93bd5a4321831c019c3a6f3
Sérgio Dantas <sergiodnts828@gmail.com>
2024-06-17 17:29:18 -0300

    chore: implement class component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-function App() {
-  return (
-    <div>
-      <h1>Olá, mundo!</h1>
-    </div>
-  );
+import React from "react";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Olá, mundo!</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
